a99262d4158f8959babdbea878ef60ebb7148ddb
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-01-30 21:51:42 +0000

    Consumer works!

--- get-consumer-init-logs.sh
+++ get-consumer-init-logs.sh
@@ -4,6 +4,6 @@ export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f6
 POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./deploy/gcp/kubeconfig.yaml`
 
 # get logs
-kubectl -n producer-consumer logs $POD_NAME --follow --kubeconfig ./deploy/gcp/kubeconfig.yaml
+kubectl -n producer-consumer logs $POD_NAME -c git-repo --follow --kubeconfig ./deploy/gcp/kubeconfig.yaml
 
 #kubectl -n producer-consumer logs $1 --previous --kubeconfig ./deploy/gcp/kubeconfig.yaml
