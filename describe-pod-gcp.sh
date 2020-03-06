7260c51b388d5570280222d18226d7aa4fca77d1
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-06 14:28:55 +0000

    Added POD_NAME env var.

--- describe-pod-gcp.sh
+++ describe-pod-gcp.sh
@@ -1,5 +1,6 @@
-export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f611b21.json
-./deploy/gcp/generate-cluster-connection-yaml.sh
+#!/bin/bash
+source ./export-gcp-credentials.sh
+./generate-cluster-connection-yaml.sh
 
-POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./deploy/gcp/kubeconfig.yaml`
-kubectl -n producer-consumer describe pods $POD_NAME --kubeconfig ./deploy/gcp/kubeconfig.yaml
+POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./kubeconfig.yaml`
+kubectl -n producer-consumer describe pods $POD_NAME --kubeconfig ./kubeconfig.yaml
--- get-logs-producer.sh
+++ get-logs-producer.sh
@@ -1,6 +1,7 @@
-export GOOGLE_APPLICATION_CREDENTIALS=./deploy/gcp/terraform-test-262517-b4ff2f611b21.json
-./deploy/gcp/generate-cluster-connection-yaml.sh
+#!/bin/bash
+source ./export-gcp-credentials.sh
+./generate-cluster-connection-yaml.sh
 
 # get the name of the producer
-PRODUCER_NAME=`kubectl -n producer-consumer get pods -o jsonpath={'$.items[1].metadata.name'} --kubeconfig ./deploy/gcp/kubeconfig.yaml`
-kubectl -n producer-consumer logs $PRODUCER_NAME --kubeconfig ./deploy/gcp/kubeconfig.yaml
+PRODUCER_NAME=`kubectl -n producer-consumer get pods -o jsonpath={'$.items[1].metadata.name'} --kubeconfig ./kubeconfig.yaml`
+kubectl -n producer-consumer logs $PRODUCER_NAME --kubeconfig ./kubeconfig.yaml
