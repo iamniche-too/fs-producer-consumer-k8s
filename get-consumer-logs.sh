488acbafb7daa49c553bb44baf5ca02cff558253
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-07 15:30:52 +0000

    Added POD_NAME as env var.

--- get-consumer-logs.sh
+++ get-consumer-logs.sh
@@ -4,6 +4,6 @@ source ./export-gcp-credentials.sh
 POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./kubeconfig.yaml`
 
 # get logs
-#kubectl -n producer-consumer logs $POD_NAME --follow --kubeconfig ./kubeconfig.yaml
+kubectl -n producer-consumer logs $POD_NAME --follow --kubeconfig ./kubeconfig.yaml
 
-kubectl -n producer-consumer logs $1 --previous --kubeconfig ./kubeconfig.yaml
+#kubectl -n producer-consumer logs $1 --previous --kubeconfig ./kubeconfig.yaml
