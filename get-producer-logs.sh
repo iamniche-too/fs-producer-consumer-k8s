27daac28b50a12dc132d8c3ece1c9e0c14613b26
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-09 16:36:06 +0000

    changed producer/consumer logging.

--- get-producer-logs.sh
+++ get-producer-logs.sh
@@ -1,9 +1,4 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
-POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[1].metadata.name}' --kubeconfig ./kubeconfig.yaml`
-
-# get logs
-kubectl -n producer-consumer logs $POD_NAME --follow --kubeconfig ./kubeconfig.yaml
-
-#kubectl -n producer-consumer logs $1 --previous --kubeconfig ./kubeconfig.yaml
+stern --namespace=producer-consumer producer.* --kubeconfig kubeconfig.yaml
