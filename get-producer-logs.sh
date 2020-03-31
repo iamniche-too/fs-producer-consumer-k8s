d8e1fd5c4148743800bf12686cc0f27a084e0a44
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-03-31 21:05:03 +0000

    Tweaked get-producer-logs.sh

--- get-producer-logs.sh
+++ get-producer-logs.sh
@@ -2,3 +2,4 @@ source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
 stern --namespace=producer-consumer producer.* --kubeconfig kubeconfig.yaml
+#kubectl -n producer-consumer logs $1 --kubeconfig kubeconfig.yaml
