07ba4dd15476cbfcce0df75ed299074e122f201f
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-04-16 09:02:50 +0100

    Tweaked get-pods for wide output.

--- get-pods.sh
+++ get-pods.sh
@@ -2,5 +2,4 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
-#kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./kubeconfig.yaml
-kubectl -n producer-consumer get pods --kubeconfig ./kubeconfig.yaml
+kubectl -n producer-consumer get pods -o wide --kubeconfig ./kubeconfig.yaml
