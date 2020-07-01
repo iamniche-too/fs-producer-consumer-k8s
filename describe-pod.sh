6ab4dfdaa680c732290cc47c6ee047f90ecc980e
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-07-01 14:16:32 +0100

    Adjusted labels for consumers & producers.

--- describe-pod.sh
+++ describe-pod.sh
@@ -2,5 +2,4 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh
 
-POD_NAME=`kubectl -n producer-consumer get pods -o jsonpath='{$.items[0].metadata.name}' --kubeconfig ./kubeconfig.yaml`
-kubectl -n producer-consumer describe pods $POD_NAME --kubeconfig ./kubeconfig.yaml
+kubectl -n producer-consumer describe pods $1 --kubeconfig ./kubeconfig.yaml
